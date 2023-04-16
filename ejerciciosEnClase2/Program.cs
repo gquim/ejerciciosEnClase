9bd7f32b67dfe47d33b1c09437e8236220bc3670
Gustavo Quim <gquimb@miumg.edu.gt>
2023-04-15 22:45:04 -0600

    Envio de directorios con los ejercicios

---
+++ ejerciciosEnClase2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingresar un numero");
+int num = int.Parse(Console.ReadLine());
+
+
+
+if (num %2==0)
+{
+    Console.WriteLine("Tu numero es par");
+}
+else
+{
+    Console.WriteLine("Tu numero es impar");
+}
+
+
+
---
+++ ejerciciosEnClase3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Precio por utilizar el estacionamiento");
+Console.WriteLine("Seleccione 1 si ha permanecido los primeros 60 min");
+Console.WriteLine("Seleccione 2 si ha permanecido las primeras 2 horas");
+Console.WriteLine("Seleccione 3 si ha permanecido más de dos horas");
+
+int number;
+int opt = int.Parse(Console.ReadLine()) ;
+
+switch (opt)
+{
+    case 1: number = 5;break;
+    case 2: number = 15; break;
+    default: number = 40; break;
+}
+Console.WriteLine($"Debe la cantidad de ${number}.00");
+
